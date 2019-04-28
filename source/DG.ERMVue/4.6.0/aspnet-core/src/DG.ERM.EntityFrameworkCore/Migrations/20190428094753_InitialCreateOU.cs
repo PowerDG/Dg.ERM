06d5fda6b04956392c8ceee351f50afd295ff353
lucifeer.wang <1049365046@qq.com>
2019-04-28 17:48:17 +0800

    InitialCreateOU

---
+++ source/DG.ERMVue/4.6.0/aspnet-core/src/DG.ERM.EntityFrameworkCore/Migrations/20190428094753_InitialCreateOU.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DG.ERM.Migrations
+{
+    public partial class InitialCreateOU : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
