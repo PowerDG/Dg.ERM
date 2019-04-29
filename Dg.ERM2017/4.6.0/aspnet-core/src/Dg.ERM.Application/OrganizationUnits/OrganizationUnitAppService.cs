6fd8ad3c98982ad41eaaa94c9d26b36fcfe267ab
lucifeer.wang <1049365046@qq.com>
2019-04-29 18:03:32 +0800

    2017

---
+++ Dg.ERM2017/4.6.0/aspnet-core/src/Dg.ERM.Application/OrganizationUnits/OrganizationUnitAppService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Abp.UI;
+using Abp.Extensions;
+using Abp.Authorization;
+using Abp.Domain.Repositories;
+using Abp.Application.Services.Dto;
+using Abp.Linq.Extensions;
+using Abp.AutoMapper;
+using Abp.Domain.Uow;
+using DG.ERM.Products;
+using DG.ERM.Products.DomainService;
+
+namespace Dg.ERM.OrganizationUnits
+{
+   public class OrganizationUnitAppService : ERMAppServiceBase
+    {
+
+        private readonly IRepository<Product, int> _entityRepository;
+
+        private readonly IProductManager _entityManager;
+
+        public OrganizationUnitAppService(
+        IRepository<Product, int> entityRepository
+        , IProductManager entityManager
+        )
+        {
+            _entityRepository = entityRepository;
+            _entityManager = entityManager;
+        }
+    }
+}
